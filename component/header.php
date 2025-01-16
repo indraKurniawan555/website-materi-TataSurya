7d12f7bf862c4a04dabce1f3326b6016f336d671
RAFazarikha <rafazarikha24@gmail.com>
2025-01-16 20:22:01 +0700

    Add header, footer, and head components for the website layout

---
+++ component/header.php
@@ -0,0 +1,33 @@
+<header id="masthead" class="site-header">
+    <nav id="primary-navigation" class="site-navigation navbar navbar-expand-lg sticky-top">
+        <div class="container">
+
+            <div class="navbar-header">
+                <a class="site-title" style="text-decoration: none;"><span>Tata</span>Surya</a>
+            </div><!-- /.navbar-header -->
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#agency-navbar-collapse" aria-controls="agency-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="agency-navbar-collapse">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php" data-toggle="dropdown">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php#planet">Sistem Tata Surya</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php#objek">Bumi dan Satelitnya</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php#fenomena">Matahari</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>   
+    </nav>
+</header>
+<!-- /#mastheaed -->
---
+++ index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <?php include 'component/head.php'; ?>
+
+</head>
+<body>
+
+    <?php include 'component/header.php'; ?>
+
+    <div id="hero" class="hero overlay">
+        <div class="hero-content">
+            <div class="hero-text">
+                <h1>Our journey of knowledge starts here.</h1>
+                <a href="#more" class="btn btn-border" style="margin-top: 45px;">Learn more</a>
+            </div><!-- /.hero-text -->
+        </div><!-- /.hero-content -->
+    </div><!-- /.hero -->
+
+    <main id="main" class="site-main">
+
+        <section id="more" class="site-section2 section-features2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h2>Tata Surya</h2>
+                        <p style="text-align: justify;">Dalam semesta penuh misteri ini, kita akan menjelajahi tiga kelompok utama yang membentuk tata surya dan fenomena di dalamnya. Dari Planet yang berkelana dalam orbitnya, hingga Objek kecil yang melintasi ruang angkasa, dan berbagai Fenomena alam yang memukau pandangan kita. Mari kita mulai memahami kedalaman alam semesta dan keajaiban yang ia tawarkan.</p>
+                    </div>
+                    <div class="col-sm-6 hidden-xs">
+                        <img src="assets/img_new/tatasurya.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </section><!-- /.section-features -->
+
+
+        <!-- Kode Untuk Bagian Planet -->
+        <section id="planet" class="site-section section-portfolio">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="heading-separator">Sistem Tata Surya</h2>
+                    <p class="subheading-text">Delapan Planet dalam Tata Surya</p>
+                </div>
+                <div class="row mt-5">
+                    <div class="row">
+                        <p class="subheading-text" style="text-align: center; font-style: normal;">Planet Terestrial</p>
+                        <div class="col-lg-6 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/merkurius.jpeg" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Merkurius</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                        <div class="col-lg-6 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/venus.jpeg" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Venus</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                        <div class="col-lg-6 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/bumi.jpeg" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Bumi</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                        <div class="col-lg-6 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/mars.jpeg" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Mars</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <p class="subheading-text" style="text-align: center; font-style: normal;">Planet Raksasa Gas</p>
+                        <div class="col-lg-6 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/jupiter.jpeg" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Jupiter</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                        <div class="col-lg-6 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/saturnus.jpeg" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Saturnus</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                        <div class="col-lg-6 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/uranus.jpeg" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Uranus</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                        <div class="col-lg-6 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/neptunus.jpeg" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Neptunus</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center">
+                    <h2 class="heading-separator"></h2>
+                    <p class="subheading-text">Benda Langit Lainnya</p>
+                </div>
+                <div class="row mt-5">
+                    <div class="row">
+                        <p class="subheading-text" style="text-align: center; font-style: normal;">Satelit</p>
+                        <div class="col-lg-4 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/ganymede.png" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Ganymede</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/titan.png" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Titan</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/io.png" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Io</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <p class="subheading-text" style="text-align: center; font-style: normal;">Planet Kerdil</p>
+                        <div class="col-lg-4 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/pluto.png" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Pluto</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/ceres.png" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Ceres</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/kecil.png" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Haumea, Makemake, dan Eris</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <p class="subheading-text" style="text-align: center; font-style: normal;">Asteroid</p>
+                        <div class="col-lg-12 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/asteroit.jpeg" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Asteroid</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <p class="subheading-text" style="text-align: center; font-style: normal;">Meteor, Meteorit, dan Meteoroid</p>
+                        <div class="col-lg-12 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/m3.png" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Meteor, Meteorit, dan Meteoroid</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <p class="subheading-text" style="text-align: center; font-style: normal;">Komet</p>
+                        <div class="col-lg-12 col-md-4 col-xs-6">
+                            <div class="portfolio-item">
+                                <img src="assets/img_new/komet.jpeg" class="img-res" alt="">
+                                <h4 class="portfolio-item-title">Komet</h4>
+                                <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                            </div><!-- /.portfolio-item -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section><!-- /.section-portfolio -->
+
+
+        <!-- Kode Untuk Bagian Objek -->
+        <section id="objek" class="site-section2 section-portfolio2">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="heading-separator2">Bumi dan Satelitnya</h2>
+                    <p class="subheading-text2">Pergerakan Bumi dalam Sistem Tata Surya</p>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 col-md-4 col-xs-6">
+                        <div class="portfolio-item2">
+                            <img src="assets/img_new/gerak-bumi1.png" class="img-res" alt="">
+                            <h4 class="portfolio-item-title2">Pergerakan Bumi dalam Sistem Tata Surya</h4>
+                            <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                        </div><!-- /.portfolio-item -->
+                    </div>
+                </div>
+                <div class="text-center">
+                    <h2 class="heading-separator2"></h2>
+                    <p class="subheading-text2">Bulan Sebagai Satelit Bumi</p>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 col-md-4 col-xs-6">
+                        <div class="portfolio-item2">
+                            <img src="assets/img_new/bulan1.png" class="img-res" alt="">
+                            <h4 class="portfolio-item-title2">Bulan Sebagai Satelit Bumi</h4>
+                            <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                        </div><!-- /.portfolio-item -->
+                    </div>
+                </div>
+                <div class="text-center">
+                    <h2 class="heading-separator2"></h2>
+                    <p class="subheading-text2">Satelit Bumi Selain Bulan</p>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 col-md-4 col-xs-6">
+                        <div class="portfolio-item2">
+                            <img src="assets/img_new/satelit.png" class="img-res" alt="">
+                            <h4 class="portfolio-item-title2">Satelit Bumi Selain Bulan</h4>
+                            <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                        </div><!-- /.portfolio-item -->
+                    </div>
+                </div>
+            </div>
+        </section><!-- /.section-portfolio -->
+
+
+        <!-- Kode Untuk Bagian Fenomena -->
+        <section id="fenomena" class="site-section section-portfolio">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="heading-separator">Mengenal Matahari Lebih Dekat</h2>
+                    <p class="subheading-text"></p>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 col-md-4 col-xs-6">
+                        <div class="portfolio-item">
+                            <img src="assets/img_new/matahari.jpeg" class="img-res" alt="">
+                            <h4 class="portfolio-item-title">Mengenal Matahari Lebih Dekat</h4>
+                            <a href="materi-item.php"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                        </div><!-- /.portfolio-item -->
+                    </div>
+                </div>
+            </div>
+        </section><!-- /.section-portfolio -->
+
+
+    </main><!-- /#main -->
+
+    <?php include 'component/footer.php'; ?>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap-select.min.js"></script>
+    <script src="assets/js/jquery.slicknav.min.js"></script>
+    <script src="assets/js/jquery.countTo.min.js"></script>
+    <script src="assets/js/jquery.shuffle.min.js"></script>
+    <script src="assets/js/script.js"></script>
+  
+</body>
+</html>
